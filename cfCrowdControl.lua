45c6dad88ab938192529fd50d49c408456421e0a
Shadeqt <itkofod@gmail.com>
2025-10-24 15:18:23 +0200

    Following new instruction guideline v2

--- cfCrowdControl.lua
+++ cfCrowdControl.lua
@@ -2,8 +2,12 @@
 cfCrowdControl = {}
 local addon = cfCrowdControl
 
+-- Localized API calls
+local _C_LossOfControl = C_LossOfControl
+local _CreateFrame = CreateFrame
+
 -- Create event frame
-local eventFrame = CreateFrame("Frame")
+local eventFrame = _CreateFrame("Frame")
 
 -- Event handler
 eventFrame:SetScript("OnEvent", function(self, event, ...)
@@ -12,7 +16,7 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 		local eventIndexOrUnit, eventIndexOrNothing = ...
 		local eventIndex = eventIndexOrNothing or eventIndexOrUnit
 
-		local data = C_LossOfControl.GetActiveLossOfControlData(eventIndex)
+		local data = _C_LossOfControl.GetActiveLossOfControlData(eventIndex)
 
 		if data and data.displayText and data.duration then
 			print(string.format("I'm %s for %.1f seconds", data.displayText, data.duration))
